e0b2ae9aa3f77631965657fcd3a3cf23884fbd62
Stuart Yamartino <stuart@yamartino.com>
2014-08-31 17:39:02 -0700

    Added validation and validation errors

--- app/controllers/AskController.php
+++ app/controllers/AskController.php
@@ -11,8 +11,28 @@ class AskController extends \BaseController {
 	{
         $name     = Input::get('name');
         $phone1   = Input::get('phone1');
-        //$phone2   = Input::get('phone2');
+        $phone2   = Input::get('phone2');
         $question = Input::get('question');
+
+        $validator = Validator::make(
+            array(
+                'name'     => $name,
+                'phone1'   => $phone1,
+                'phone2'   => $phone2,
+                'question' => $question,
+            ),
+            array(
+                'name' => 'required',
+                'phone1' => 'required',
+                'phone2' => 'required',
+                'question' => 'required',
+            )
+        );
+
+        if ($validator->fails())
+        {
+            return Redirect::to('/')->withErrors($validator);
+        }
 
         Sms::send(array('to'=>$phone1, 'text'=>$name.' asked the question: '.$question. 'Answer: 100% Yes!'));
         return 'tst';
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Ask Twilio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:100">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css" />
     <!--[if lt IE 9]>
@@ -16,9 +17,13 @@
       <div class="col-md-4">
           <h1 class="center-text">Ask Twilio</h1>
           <form action="" method="post">
+              <h5>{{{ $errors->first('name') }}}</h5>
               <input class="form-control" type="text" name="name" placeholder="Your Name"/>
+              <h5>{{{ $errors->first('phone1') }}}</h5>
               <input class="form-control" type="tel"  name="phone1" placeholder="Your Phone #"/>
+              <h5>{{{ $errors->first('phone2') }}}</h5>
               <input class="form-control" type="tel"  name="phone2" placeholder="Your Friends Phone #"/>
+              <h5>{{{ $errors->first('question') }}}</h5>
               <textarea class="form-control" name="question" placeholder="Ask a question..." rows="3"></textarea>
               <input class="btn btn-danger center-text" type="submit" value="Ask" />
           </form>
