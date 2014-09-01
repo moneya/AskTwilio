78a5f5b598315ee9a429d8b94f73ce2c6c5dc7e7
Stuart Yamartino <stuart@yamartino.com>
2014-08-31 17:49:41 -0700

    Added switch case of answers

--- app/controllers/AskController.php
+++ app/controllers/AskController.php
@@ -34,18 +34,59 @@ class AskController extends \BaseController {
             return Redirect::to('/')->withErrors($validator);
         }
 
-        Sms::send(array('to'=> $phone1, 'text'=> $name.' asked the question: '.$question. ' Answer: 100% Yes!'));
-        Sms::send(array('to'=> $phone2, 'text'=> $name.' asked the question: '.$question. ' Answer: 100% Yes!'));
+        $answer = $this->getAnswer();
+
+        Sms::send(array('to'=> $phone1, 'text'=> $name.' asked the question: '.$question. ' Answer: '.$answer));
+        Sms::send(array('to'=> $phone2, 'text'=> $name.' asked the question: '.$question. ' Answer: '.$answer));
         return 'tst';
 	}
 
-    private function formatNumber($number){
+    private function formatNumber($number)
+    {
         $number = preg_replace("/[^0-9]/", "", $number);
         if(substr($number, 0, 1) === 1 )
         {
             return '+'.$number;
         }
         return '+1'.$number;
+    }
+
+    private function getAnswer()
+    {
+        $random = rand(1,10);
+
+        switch ($random) {
+            case 1:
+                return '100% Yes!';
+                break;
+            case 2:
+                return "Let's hope not...";
+                break;
+            case 3:
+                return "Don't count on it!";
+                break;
+            case 4:
+                return "Seems that way.";
+                break;
+            case 5:
+                return "Without a dout!";
+                break;
+            case 6:
+                return "Meh...";
+                break;
+            case 7:
+                return "Hahaha whatever you say...";
+                break;
+            case 8:
+                return "Hard to tell.";
+                break;
+            case 9:
+                return "Weird question :/";
+                break;
+            case 10:
+                return "Better not tell you.";
+                break;
+        }
     }
 
 }
