df127e08ad3c95039f34d1a834d8771e63fc6991
Stuart Yamartino <stuart@yamartino.com>
2014-08-31 17:26:54 -0700

    Adding style to page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,19 +5,25 @@
     <title>Ask Twilio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/prism.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
   </head>
   <body>
-    <h1>Ask Twilio</h1>
-    <form action="" method="post">
-        <input class="form-control" type="text" name="name" placeholder="Your Name"/>
-        <input class="form-control" type="tel"  name="phone1" placeholder="Your Phone #"/>
-        <input class="form-control" type="tel"  name="phone2" placeholder="Your Friends Phone #"/>
-        <input class="form-control" type="text" name="question" placeholder="Ask a question..."/>
-        <input class="btn btn-default" type="submit" value="Ask" />
-    </form>
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+          <h1 class="center-text">Ask Twilio</h1>
+          <form action="" method="post">
+              <input class="form-control" type="text" name="name" placeholder="Your Name"/>
+              <input class="form-control" type="tel"  name="phone1" placeholder="Your Phone #"/>
+              <input class="form-control" type="tel"  name="phone2" placeholder="Your Friends Phone #"/>
+              <textarea class="form-control" name="question" placeholder="Ask a question..." rows="3"></textarea>
+              <input class="btn btn-danger center-text" type="submit" value="Ask" />
+          </form>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
   </body>
 </html>
